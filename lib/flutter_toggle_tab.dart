654110c32a2f26c69254ee801811dbd44476579e
Abdelrahman Saed <me@abdosaed.ml>
2021-08-08 17:20:54 -0700

    add border

--- lib/flutter_toggle_tab.dart
+++ lib/flutter_toggle_tab.dart
@@ -20,25 +20,25 @@ class FlutterToggleTab extends StatefulWidget {
   /// Define parameter Flutter toggle tab
   /// It's main attribute is available on Flutter Toggle Tab
   /// is Scroll by default is set to Enable
-  const FlutterToggleTab(
-      {Key? key,
-      required this.labels,
-      required this.initialIndex,
-      required this.selectedLabelIndex,
-      required this.selectedTextStyle,
-      required this.unSelectedTextStyle,
-      this.height,
-      this.icons,
-      this.selectedBackgroundColors,
-      this.unSelectedBackgroundColors,
-      this.width,
-      this.borderRadius,
-      this.begin,
-      this.end,
-      this.selectedIndex,
-      this.isScroll = true,
-      this.marginSelected,
-      this.isShadowEnable = true})
+  const FlutterToggleTab({Key? key,
+    required this.labels,
+    required this.initialIndex,
+    required this.selectedLabelIndex,
+    required this.selectedTextStyle,
+    required this.unSelectedTextStyle,
+    this.height,
+    this.icons,
+    this.selectedBackgroundColors,
+    this.unSelectedBackgroundColors,
+    this.width,
+    this.borderRadius,
+    this.begin,
+    this.end,
+    this.selectedIndex,
+    this.isScroll = true,
+    this.marginSelected,
+    this.border,
+    this.isShadowEnable = true})
       : super(key: key);
 
   final List<String> labels;
@@ -48,6 +48,7 @@ class FlutterToggleTab extends StatefulWidget {
   final double? width;
   final double? height;
   final bool isScroll;
+  final BoxBorder border;
 
 //  final BoxDecoration selectedDecoration;
 //  final BoxDecoration unSelectedDecoration;
@@ -109,108 +110,117 @@ class _FlutterToggleTabState extends State<FlutterToggleTab> {
     /// Show text error if length less 1
     return widget.labels.length <= 1
         ? Text(
-            "Error : Label should >1",
-            style: TextStyle(
-                color: Colors.redAccent,
-                fontWeight: FontWeight.bold,
-                fontSize: 20),
-          )
+      "Error : Label should >1",
+      style: TextStyle(
+          color: Colors.redAccent,
+          fontWeight: FontWeight.bold,
+          fontSize: 20),
+    )
         : Container(
-            width: width,
-            height: widget.height ?? 45,
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  // Where the linear gradient begins and ends
-                  begin: widget.begin ?? Alignment.topCenter,
-                  end: widget.end ?? Alignment.bottomCenter,
+        width: width,
+        height: widget.height ?? 45,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              // Where the linear gradient begins and ends
+              begin: widget.begin ?? Alignment.topCenter,
+              end: widget.end ?? Alignment.bottomCenter,
 
-                  /// If unSelectedBackground is not null
-                  /// We check again if it's length only 1
-                  /// Using same color for gradients
-                  colors: widget.unSelectedBackgroundColors != null
-                      ? (widget.unSelectedBackgroundColors!.length == 1
-                          ? [
-                              widget.unSelectedBackgroundColors![0],
-                              widget.unSelectedBackgroundColors![0]
-                            ]
-                          : widget.unSelectedBackgroundColors!)
-                      : [Color(0xffe0e0e0), Color(0xffe0e0e0)],
-                ),
-                borderRadius: BorderRadius.circular(widget.borderRadius ?? 30),
+              /// If unSelectedBackground is not null
+              /// We check again if it's length only 1
+              /// Using same color for gradients
+              colors: widget.unSelectedBackgroundColors != null
+                  ? (widget.unSelectedBackgroundColors!.length == 1
+              ? [
+              widget.unSelectedBackgroundColors![0],
+                  widget.unSelectedBackgroundColors![0]
+                  ]
+                      : widget.unSelectedBackgroundColors!)
+                  : [Color(0xffe0e0e0), Color(0xffe0e0e0)],
+            ),
+            borderRadius: BorderRadius.circular(widget.borderRadius ?? 30),
 
-                /// Handle if shadow is Enable or not
-                boxShadow: [if (widget.isShadowEnable) bsInner]),
-            child: ListView.builder(
-              itemCount: _labels.length,
+            border: border ?? Border.all(color: Theme
+                .of(context)
+                .primaryColor),
 
-              /// Handle if isScroll or not
-              physics: widget.isScroll
-                  ? BouncingScrollPhysics()
-                  : NeverScrollableScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                IconData? icon;
+            /// Handle if shadow is Enable or not
+            boxShadow: [if (widget.isShadowEnable) bsInner]),
+        child: ListView.builder(
+          itemCount: _labels.length,
 
-                // Using try catch to fix error Range array
+          /// Handle if isScroll or not
+          physics: widget.isScroll
+              ? BouncingScrollPhysics()
+              : NeverScrollableScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            IconData? icon;
+
+            // Using try catch to fix error Range array
+            try {
+              icon = widget.icons?[index];
+            } catch (e) {
+              icon = null;
+            }
+
+            return ButtonsTab(
+              marginSelected: widget.marginSelected,
+
+              /// If unSelectedBackground is not null
+              /// We check again if it's length only 1
+              /// Using same color for gradients
+              unSelectedColors: widget.unSelectedBackgroundColors != null
+                  ? (widget.unSelectedBackgroundColors!.length == 1
+              ? [
+              widget.unSelectedBackgroundColors![0],
+                  widget.unSelectedBackgroundColors![0]
+                  ]
+                      : widget.unSelectedBackgroundColors)
+                  : [Color(0xffe0e0e0), Color(0xffe0e0e0)],
+              width: width / widget.labels.length,
+              title: _labels[index].title,
+              icons: icon,
+              selectedTextStyle: widget.selectedTextStyle,
+              unSelectedTextStyle: widget.unSelectedTextStyle,
+              isSelected: _labels[index].isSelected,
+              radius: widget.borderRadius ?? 30,
+
+              /// If selectedBackgroundColors is not null
+              /// We check again if it's length only 1
+              /// Using same color for gradients
+              selectedColors: widget.selectedBackgroundColors != null
+                  ? (widget.selectedBackgroundColors!.length == 1
+              ? [
+              widget.selectedBackgroundColors![0],
+                  widget.selectedBackgroundColors![0]
+                  ]
+                      : widget.selectedBackgroundColors)
+                  : [
+                Theme
+                    .of(context)
+                    .primaryColor,
+                Theme
+                    .of(context)
+                    .primaryColor
+              ],
+              onPressed: () {
                 try {
-                  icon = widget.icons?[index];
+                  for (int x = 0; x < _labels.length; x++) {
+                    setState(() {
+                      if (_labels[index] == _labels[x]) {
+                        _labels[x].isSelected = true;
+                        widget.selectedLabelIndex(index);
+                      } else
+                        _labels[x].isSelected = false;
+                    });
+                  }
                 } catch (e) {
-                  icon = null;
+                  print("err : $e");
                 }
-
-                return ButtonsTab(
-                  marginSelected: widget.marginSelected,
-
-                  /// If unSelectedBackground is not null
-                  /// We check again if it's length only 1
-                  /// Using same color for gradients
-                  unSelectedColors: widget.unSelectedBackgroundColors != null
-                      ? (widget.unSelectedBackgroundColors!.length == 1
-                          ? [
-                              widget.unSelectedBackgroundColors![0],
-                              widget.unSelectedBackgroundColors![0]
-                            ]
-                          : widget.unSelectedBackgroundColors)
-                      : [Color(0xffe0e0e0), Color(0xffe0e0e0)],
-                  width: width / widget.labels.length,
-                  title: _labels[index].title,
-                  icons: icon,
-                  selectedTextStyle: widget.selectedTextStyle,
-                  unSelectedTextStyle: widget.unSelectedTextStyle,
-                  isSelected: _labels[index].isSelected,
-                  radius: widget.borderRadius ?? 30,
-
-                  /// If selectedBackgroundColors is not null
-                  /// We check again if it's length only 1
-                  /// Using same color for gradients
-                  selectedColors: widget.selectedBackgroundColors != null
-                      ? (widget.selectedBackgroundColors!.length == 1
-                          ? [
-                              widget.selectedBackgroundColors![0],
-                              widget.selectedBackgroundColors![0]
-                            ]
-                          : widget.selectedBackgroundColors)
-                      : [
-                          Theme.of(context).primaryColor,
-                          Theme.of(context).primaryColor
-                        ],
-                  onPressed: () {
-                    try {
-                      for (int x = 0; x < _labels.length; x++) {
-                        setState(() {
-                          if (_labels[index] == _labels[x]) {
-                            _labels[x].isSelected = true;
-                            widget.selectedLabelIndex(index);
-                          } else
-                            _labels[x].isSelected = false;
-                        });
-                      }
-                    } catch (e) {
-                      print("err : $e");
-                    }
-                  },
-                );
               },
-            ));
+            );
+          },
+        )
+    );
   }
 }
